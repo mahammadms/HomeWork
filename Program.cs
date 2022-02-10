9d64e9cf2cb734d34b8daa31d06af3730a7daf89
magamadeit <mahammadms@code.edu.az>
2022-02-10 09:08:32 +0400

    added file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿#region Task 2
+//string[] words = { "Car", "Machine", "Vehicle", "Bus", "Subway" };
+//foreach (string v in words)
+//{
+//    Console.WriteLine(v);
+//}
+#endregion
+
+
+int num = 325;
+if (num / 10 == 0)
+{
+    for (int i = 0; i < length; i++)
+    {
+        
+    }
+}
